4faf161fdd0a7a0aff99e6d214b9dd9c2b149af9
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-13 17:42:28 +0200

    Update generator

--- lib/generators/the_bullet/the_bullet_generator.rb
+++ lib/generators/the_bullet/the_bullet_generator.rb
@@ -1,17 +1,25 @@
 class TheBulletGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-  class_option :base_controller, type: :boolean, default: true, desc: 'Generate BaseController'
+  class_option :base, type: :boolean, default: true, desc: 'Generate base stuff. Base controller and views.'
 
-  class_option :sign_up, type: :boolean, default: true, desc: 'Generate SignUpsController'
+  class_option :sign_up, type: :boolean, default: true, desc: 'Generate user sign up feature'
 
-  class_option :sign_in, type: :boolean, default: true, desc: 'Generate SignInsController'
+  class_option :sign_in, type: :boolean, default: true, desc: 'Generate user sign in feature'
 
-  class_option :sign_out, type: :boolean, default: true, desc: 'Generate SignOutsController'
+  class_option :sign_out, type: :boolean, default: true, desc: 'Generate user sign out feature'
+
+  class_option :password_reset, type: :boolean, default: true, desc: 'Generate user password reset feature'
+
+  class_option :profile, type: :boolean, default: true, desc: 'Generate user profile'
 
   class_option :pundit, type: :boolean, default: true, desc: 'Generate pundit stuff'
 
   class_option :swagger, type: :boolean, default: true, desc: 'Generate Swagger documentation'
+
+  class_option :backoffice, type: :boolean, default: true, desc: 'Generate Backoffice'
+
+  class_option :backoffice_users, type: :boolean, default: true, desc: 'Generate Backoffice users'
 
   def generate_base_controller
     puts "generate base controller"
