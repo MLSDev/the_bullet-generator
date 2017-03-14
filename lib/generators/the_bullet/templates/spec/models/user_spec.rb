4f1f2a2b6c3d185e70e524fe442e798edaa93256
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-14 12:42:55 +0200

    Add specs templates for models and decorators

---
+++ lib/generators/the_bullet/templates/spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe User do
+  it { should be_a(ApplicationRecord) }
+
+  it { should have_many(:sessions).dependent(:destroy) }
+
+  it { should validate_presence_of(:email) }
+
+  it { should validate_uniqueness_of(:email).case_insensitive }
+
+  it { should have_secure_password }
+
+  pending { should have_secure_token(:reset_token) }
+end
