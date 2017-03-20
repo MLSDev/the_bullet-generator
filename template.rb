817629616d352f30a34240f8b09499f2559003fa
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 17:39:28 +0200

    Add rails template

---
+++ template.rb
@@ -0,0 +1,39 @@
+# # gem 'rails', '~> 5.0.2'
+# gem 'sqlite3'
+# gem 'puma'
+# gem 'bcrypt'
+# gem 'redis'
+# gem 'redis-objects'
+# gem 'rack-cors', require: 'rack/cors'
+# gem 'kaminari'
+# gem 'dotenv-rails'
+# gem 'draper', '~> 3.0.0.pre1'
+# gem 'pundit'
+# gem 'sidekiq'
+# gem 'rpush'
+# gem 'paperclip'
+# gem 'paperclip-meta'
+# gem 'swagger-blocks'
+# gem 'lograge', group: :production
+# gem 'exception_notification', group: :production
+# gem 'rack-timeout', group: :production
+# gem 'newrelic_rpm', group: :production
+# gem 'rspec-rails', group: [:development, :test]
+# gem 'pry-rails', group: [:development, :test]
+# gem 'awesome_print', require: 'ap', group: [:development, :test]
+# gem 'faker', group: [:development, :test]
+# gem 'factory_girl_rails', group: [:development, :test]
+# # gem 'listen', '~> 3.0.5', group: :development
+# gem 'rubocop', '0.47.1', require: false, group: :development
+# gem 'capistrano', group: :development
+# gem 'capistrano-rails', group: :development
+# gem 'rails-controller-testing', group: :test
+# gem 'shoulda-matchers', group: :test
+# gem 'rspec-activemodel-mocks', group: :test
+# gem 'rspec-its', group: :test
+# gem 'shoulda-callback-matchers', group: :test
+# gem 'simplecov', require: false, group: :test
+# gem 'database_cleaner', group: :test
+
+gem 'dotenv-rails'
+gem 'kaminari'
