38b3184fee0ff025bf84c39388f21645f62df878
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-21 15:23:39 +0200

    Update template

--- template.rb
+++ template.rb
@@ -1,39 +1,64 @@
-# # gem 'rails', '~> 5.0.2'
-# gem 'sqlite3'
-# gem 'puma'
-# gem 'bcrypt'
-# gem 'redis'
-# gem 'redis-objects'
-# gem 'rack-cors', require: 'rack/cors'
-# gem 'kaminari'
-# gem 'dotenv-rails'
-# gem 'draper', '~> 3.0.0.pre1'
-# gem 'pundit'
-# gem 'sidekiq'
-# gem 'rpush'
-# gem 'paperclip'
-# gem 'paperclip-meta'
-# gem 'swagger-blocks'
-# gem 'lograge', group: :production
-# gem 'exception_notification', group: :production
-# gem 'rack-timeout', group: :production
-# gem 'newrelic_rpm', group: :production
-# gem 'rspec-rails', group: [:development, :test]
-# gem 'pry-rails', group: [:development, :test]
-# gem 'awesome_print', require: 'ap', group: [:development, :test]
-# gem 'faker', group: [:development, :test]
-# gem 'factory_girl_rails', group: [:development, :test]
-# # gem 'listen', '~> 3.0.5', group: :development
-# gem 'rubocop', '0.47.1', require: false, group: :development
-# gem 'capistrano', group: :development
-# gem 'capistrano-rails', group: :development
-# gem 'rails-controller-testing', group: :test
-# gem 'shoulda-matchers', group: :test
-# gem 'rspec-activemodel-mocks', group: :test
-# gem 'rspec-its', group: :test
-# gem 'shoulda-callback-matchers', group: :test
-# gem 'simplecov', require: false, group: :test
-# gem 'database_cleaner', group: :test
-
-gem 'dotenv-rails'
+gem 'bcrypt'
+gem 'redis'
+gem 'redis-objects'
+gem 'rack-cors', require: 'rack/cors'
 gem 'kaminari'
+gem 'dotenv-rails'
+gem 'draper', '~> 3.0.0.pre1'
+
+# sign in/sign out/etc
+authentication = yes?('Include authentication? Sign in, sign out, etc.')
+
+authorization = nil
+
+if authentication
+  # pundit
+  authorization = yes?('Include permissions stuff?')
+end
+
+if authorization
+  gem 'pundit'
+end
+
+sidekiq = yes?('Include sidekiq?')
+
+if sidekiq
+  gem 'sidekiq'
+end
+
+push_notifications = yes?('Include push notifications?')
+
+if push_notifications
+  gem 'rpush'
+end
+
+swagger = yes?('Include swagger?')
+
+if swagger
+  gem 'swagger-blocks'
+end
+
+gem 'paperclip'
+gem 'paperclip-meta'
+
+gem_group :production do
+  gem 'lograge'
+  gem 'exception_notification'
+  gem 'rack-timeout'
+  gem 'newrelic_rpm'
+end
+
+rspec = yes?('Include rspec?')
+
+gem_group :development, :test do
+  gem 'rspec-rails' if rspec
+  gem 'pry-rails'
+  gem 'awesome_print', require: 'ap'
+  gem 'faker'
+  gem 'factory_girl_rails' if rspec
+end
+
+gem_group :development do
+  # rubocop version locked due config. Update rubocop config on gem update.
+  gem 'rubocop', '0.47.1', require: false
+end
